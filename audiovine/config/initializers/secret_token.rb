4014f6f3cb1419b41b963abb1bdb8263ac4d518e
jon hasz <jon@millerag.com>
2013-01-14 21:08:31 -0800

    first commit

---
+++ audiovine/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audiovine::Application.config.secret_token = '04473d5752d709ec35571bd3c6bbdc83e505f54f39639b1602c2d47130a9f54e4791f16589af4ef120568d91df5b8ab74f3ab06366eeee800ecc3e26d354cf6b'
